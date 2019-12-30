f29005f99e98026a4752a2c3ed9dfdca6313ef91
Thalles Veloso <thalleslima8@gmail.com>
2019-12-30 15:55:30 -0300

    Criando regras de xeque

--- Tabuleiro/Peca.cs
+++ Tabuleiro/Peca.cs
@@ -18,6 +18,10 @@ namespace tabuleiro {
             qtdMovimentos++;
         }
 
+        public void decrementarQtdMovimentos() {
+            qtdMovimentos--;
+        }
+
         public bool existeMovimentosPossiveis() {
             bool[,] mat = movimentosPossiveis();
             for (int i = 0; i < tab.linhas; i++) {
